7b3dce5f8d458d61e505738def7e569bad34b631
MarwanIssa100 <missa4312@gmail.com>
2025-07-02 23:32:06 +0300

    edit task 0  adv database

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,6 +1,6 @@
 use alx_airbnb;
-select first_name , last_name , booking_id from user as u inner join booking as o ON u.user_id = o.user_id;
-select p_name , r_comment from property as p left join review as r on p.property_id = r.property_id ;
+select first_name , last_name , booking_id from user as u INNER JOIN booking as o ON u.user_id = o.user_id;
+select p_name , r_comment from property as p LEFT JOIN review as r on p.property_id = r.property_id ;
 SELECT u.first_name, u.last_name, o.booking_id
 FROM user u
 LEFT JOIN booking o ON u.user_id = o.user_id
